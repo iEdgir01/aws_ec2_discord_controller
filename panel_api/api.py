cbc01f5ebec9265f0d236c57f52fc5624432d212
iEdgir01 <fgteiw@gmail.com>
2022-06-17 14:57:11 +0200

    outlined next dev steps for the api functionality

--- panel_api/api.py
+++ panel_api/api.py
@@ -129,6 +129,16 @@ def getServerNameFromId():
                         server_data[f'{name}'].update(token)
     return server_data
 
+# next steps are as follows, may not be in order
+# a) create a websocket call and determine if server is on or off. - update server details with status
+# b) if server on take port and pass it to the bot to work with checking the server state.
+# c) create functionality to post node and server states in discord.
+# d) create flags to handle starting the Instance and the server flagged
+# e) stop function to stop server then the instance
+# f) notifications every 2 hours on node uptime
+# g) server uptime & node uptime calculations
+# h) investigate discord bot gui to do these actions without needing to type commands in dc chat.. 
+
 print(getServerNameFromId())
             
         
